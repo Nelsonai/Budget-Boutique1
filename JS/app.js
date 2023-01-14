62284f8d8d88eeefe5b787ea486b4872211b31cf
olisa <94673683+Olisa02@users.noreply.github.com>
2023-01-14 08:28:35 -0700

    design update

--- JS/app.js
+++ JS/app.js
@@ -11,27 +11,3 @@ const navSlide=()=>{
 navSlide()
 
 
-// Wait for the window to load
-window.onload = function() {
-    // Select all elements with the class 'kids'
-    const textElements = document.querySelectorAll('.kids');
-  
-    // Create a new IntersectionObserver
-    const observer = new IntersectionObserver((entries) => {
-      entries.forEach((entry) => {
-        // Check if the text element is visible in the viewport
-        if (entry.intersectionRatio > 0) {
-          // Add the 'active' class to the text element to start the animation
-          entry.target.classList.add('active');
-        } else {
-          // Remove the 'active' class to stop the animation
-          entry.target.classList.remove('active');
-        }
-      });
-    });
-  
-    // Iterate through all text elements and observe each one
-    textElements.forEach((textElement) => {
-      observer.observe(textElement);
-    });
-};
